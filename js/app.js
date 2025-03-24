f115ca754e52b901e6e7b2fcd9b4e02bb1e2f370
MrRacoonGH <hadrien181107@gmail.com>
2025-03-24 17:03:57 +0100

    update-css/htmll/api/j.son

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+const $login = document.querySelector('.login')
+const $bio = document.querySelector('.bio')
+const $repositories = document.querySelector('')
+const $followers = document.querySelector('')
+const $following = document.querySelector('')
+const $city = document.querySelector('')
+const $socialLink = document.querySelector('')
+const $githhubLink = document.querySelector('')
+const $githubProfil = document.querySelector('')
+
 
 fetch(`https://api.github.com/users/${search-input}`,{
     headers:{
@@ -16,3 +26,4 @@ fetch(`https://api.github.com/users/${search-input}`,{
   .catch(error => {
     console.error(error);
   });
+
